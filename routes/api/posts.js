c7eb76efcc07fe291e7479210ee71e76f734f5d8
Muhammad Umar <muhammadumer051999@gmail.com>
2023-02-19 18:12:18 +0000

    Added Posts Schema and Create Post endpoint

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,45 @@
 const express = require("express");
-
 const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => {
-  res.send("Posts Route");
-});
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+const Post = require("../../models/Post");
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/",
+  [auth],
+  [check("text", "Text is required").not().isEmpty()],
+  async (req, res) => {
+    // Check for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const postFields = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      // Create a new Post
+      const post = new Post(postFields);
+      // Save the Post
+      await post.save();
+      // Return the Post
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
