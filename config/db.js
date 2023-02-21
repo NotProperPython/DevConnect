611bc1d61e340104a9a8b64cf57ebd88ffec5d05
Muhammad Umar <muhammadumer051999@gmail.com>
2023-02-21 00:25:44 +0000

    Added a readme + some more comments

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
   try {
     // By default is set to 'false' but we need 'true'
     mongoose.set("strictQuery", true);
+    // By default is set to 'true' but we need 'false'
     mongoose.set("strictPopulate", false);
     await mongoose.connect(db);
     console.log("DB connected...");
