4ee3de4e9ea9e19634a52f48a18769fd6670f5f2
Muhammad Umar <muhammadumer051999@gmail.com>
2023-02-26 18:48:04 +0000

    started working on profileById page

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -40,6 +41,7 @@ const App = () => {
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profiles" element={<Profiles />} />
+            <Route path="/profile/:id" element={<Profile />} />
 
             <Route element={<PrivateRoutes />}>
               <Route path="/dashboard" element={<Dashboard />} />
---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import Spinner from "../layout/Spinner";
+import { connect } from "react-redux";
+
+import { getProfileById } from "../../actions/profile";
+import { Link, useParams } from "react-router-dom";
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
+
+const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
+  const { id } = useParams();
+  useEffect(() => {
+    getProfileById(id);
+  }, [getProfileById]);
+
+  return (
+    <>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Link to="/profiles" className="btn btn-light">
+            Back To Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+          <div class="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getProfileById })(Profile);
