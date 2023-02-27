90622c3dc7ab8a7a47a8b32744991e256077b220
Muhammad Umar <muhammadumer051999@gmail.com>
2023-02-27 22:35:31 +0000

    like and unlike func added for Posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,40 +3,52 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
+import { addLike, removeLike } from "../../actions/post";
 
 const PostItem = ({
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
+  addLike,
+  removeLike,
 }) => {
   return (
-    <div class="post bg-white p-1 my-1">
+    <div className="post bg-white p-1 my-1">
       <div>
         <Link to={`/profile/${user}`}>
-          <img class="round-img" src={avatar} alt="" />
+          <img className="round-img" src={avatar} alt="" />
           <h4>{name}</h4>
         </Link>
       </div>
       <div>
-        <p class="my-1">{text}</p>
-        <p class="post-date">
+        <p className="my-1">{text}</p>
+        <p className="post-date">
           Posted on <Moment format="DD/MM/YYYY">{date}</Moment>
         </p>
-        <button type="button" class="btn btn-light">
-          <i class="fas fa-thumbs-up"></i>{" "}
+        {/* TODO: Highlight this button if liked by the current user */}
+        <button
+          onClick={(e) => addLike(_id)}
+          type="button"
+          className="btn btn-light"
+        >
+          <i className="fas fa-thumbs-up"></i>{" "}
           {likes.length > 0 && <span>{likes.length}</span>}
         </button>
-        <button type="button" class="btn btn-light">
-          <i class="fas fa-thumbs-down"></i>
+        <button
+          onClick={(e) => removeLike(_id)}
+          type="button"
+          className="btn btn-light"
+        >
+          <i className="fas fa-thumbs-down"></i>
         </button>
-        <Link href={`/post/${_id}`} class="btn btn-primary">
+        <Link href={`/post/${_id}`} className="btn btn-primary">
           Discussion{" "}
           {comments.length > 0 && (
-            <span class="comment-count">{comments.length}</span>
+            <span className="comment-count">{comments.length}</span>
           )}
         </Link>
         {!auth.loading && user === auth.user._id && (
-          <button type="button" class="btn btn-danger">
-            <i class="fas fa-times"></i>
+          <button type="button" className="btn btn-danger">
+            <i className="fas fa-times"></i>
           </button>
         )}
       </div>
@@ -47,10 +59,12 @@ const PostItem = ({
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
